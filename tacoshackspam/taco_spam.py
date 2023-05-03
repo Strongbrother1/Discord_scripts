a312df52d85f35e250b3673a1f6ee501e89bd2ad
Strongbrother1 <calvinmaycelee@gmail.com>
2023-05-03 12:14:44 -0500

    changed functions

--- tacoshackspam/taco_spam.py
+++ tacoshackspam/taco_spam.py
@@ -2,25 +2,29 @@ import pyautogui, time
 """
 Basic Program to spam discord "TacoShack" bot
 """
+# Functions to use in loop
+def work():
+    pyautogui.typewrite("/work")
+    time.sleep(1)
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+
+def tip():
+    pyautogui.typewrite("/tips")
+    time.sleep(1)
+    pyautogui.press("enter")
+    pyautogui.press("enter")
+
 # Sleep to tab into program
 time.sleep(5)
 i,k = 0,1
 # Infinite loop, do ctrl + c to quit in terminal
 while i < k:
-    pyautogui.typewrite("/work")
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.press("enter")
+    work()
     time.sleep(2)
-    pyautogui.typewrite("/tips")
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.press("enter")
+    tip()
     # Sleep 5 minutes + 2 seconds to account for latency/lag
     time.sleep(5 * 60 + 2)
-    pyautogui.typewrite("/tips")
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.press("enter")
+    tip()
     # Sleep 5 minutes + 2 seconds to account for latency/lag
     time.sleep(5 * 60 + 2)
